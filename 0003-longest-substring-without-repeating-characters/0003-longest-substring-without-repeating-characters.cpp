44fbec4cc6a2af11f8823f1744d5849a0683c3d1
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2025-07-26 12:30:49 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
 
-        int left = 0;
-        unordered_set<char> mySet;
-
-        int count = 0;
-
-        for(int i = 0; i < s.size(); i++){
-
-            while(mySet.count(s[i])){
-               mySet.erase(s[left]);
-               left++;
-            }
-
-            mySet.insert(s[i]);
-            
-            count = max(count, i-left+1);
-
+        if(s.size()==0){
+            return 0;
         }
 
-        return count;
+        if(s.size()==1){
+            return 1;
+        }
+
+        unordered_set<char> mySet;
+        int length = 0;
+
+        // we can use sliding window technique
+        int left = 0;
+
+        for(int i = 0; i < s.size(); i++){ // i is right
+
+            if(mySet.count(s[i])){
+                length = max(length, i - left);
+
+                while((s[left] != s[i]) && (left < i)){
+                    left++;
+                }
+                left++;
+            }else{
+                mySet.insert(s[i]);
+            }
+
+            // while(mySet.count(s[i])){
+            //     left++;
+            //     length = 0;
+            // }
+
+            
+
+            // t a b c a
+            // left = t
+
+        }
+        return length;
         
     }
 };
