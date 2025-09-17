9b1c9db90865e43deca5bee21ea5729829da4230
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2025-09-17 11:22:49 -0400

    Time: 50 ms (59.84%), Space: 90.8 MB (46.51%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int> mySet;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(mySet.count(nums[i])){
+                return true;
+            }
+            mySet.insert(nums[i]);
+        }
+
+        return false;
+        
+    }
+};
