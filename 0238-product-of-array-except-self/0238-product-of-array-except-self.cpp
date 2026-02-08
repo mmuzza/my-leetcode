1c42c9dbc3d2400d5c3f4d2d34b4e00d72e2f8ff
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-02-08 11:05:37 -0600

    Time: 4 ms (21.02%), Space: 47 MB (5.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,43 +1,51 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+
+        // Input: nums = [1,2,3,4]
+        // Output: [24,12,8,6]
+
+        // nums: 1 2 3 4
+        // left = 1 1 2 6
+
+        // nums: 1 2 3 4
+        // right = 24 12 4 1
+
+        // left = 1 1 2 6
+        // right = 24 12 4 1
+
+        // output: left * right = 24 12 8 6
+
+        vector<int> left;
+        vector<int> right;
+        vector<int> output;
         
-        // nums =  1,  2,  3,  4
-        // left =  1,  2,  6,  24
-        // right = 24, 24, 12,  4
-
-        vector<int> result;
-        vector<int> leftSum;
-        vector<int> rightSum;
-
+        right.push_back(1);
+        left.push_back(1);
         int product = 1;
-        for(int i = 0; i < nums.size(); i++){
-            product = product * nums[i];
-            leftSum.push_back(product);
 
-            rightSum.push_back(0);
+        for(int i = 1; i < nums.size(); i++){
+            product = nums[i-1] * product;
+            left.push_back(product);
+
+            right.push_back(1);
         }
+        
 
+
+        right[nums.size()-1] = 1;
         product = 1;
-        for(int i = nums.size()-1; i >= 0; i--){
-            product = product * nums[i];
-            rightSum[i] = product;
+
+        for(int i = nums.size()-2; i >= 0; i--){
+            product = nums[i+1] * product;
+            right[i] = product;
         }
 
         for(int i = 0; i < nums.size(); i++){
-
-            if(i == 0){
-                result.push_back(rightSum[i+1]);
-                continue;
-            }
-            if(i == nums.size()-1){
-                result.push_back(leftSum[i-1]);
-                continue;
-            }
-
-            result.push_back(leftSum[i-1] * rightSum[i+1]);
+            output.push_back(left[i] * right[i]);
         }
 
-        return result;
+        return output;
+
     }
 };
