5ea05b4151eafb43dcfd22cfbf0f400ee62e0ae7
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-02-08 10:50:09 -0600

    Time: 5 ms (25.05%), Space: 18.1 MB (40.56%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-
-        // create a frequency map
-        // use priority queue to get k amount
-
-        unordered_map<int, int> myMap;
-
-        for(int i = 0; i < nums.size(); i++){
-            myMap[nums[i]]++;
-        }
-
-        priority_queue<pair<int,int>> q;
-
-        for(const auto & p : myMap){
-            q.push({p.second,p.first});
-        }
-
         
+        // Store the count inside of a map
+        // so if  nums = [1,1,1,2,2,3] we will have:
+            // 1 : 3
+            // 2 : 2
+            // 3 : 1
+
+        unordered_map<int, int> numsFreq;
+        for(int i = 0; i < nums.size(); i++){
+            numsFreq[nums[i]]++;
+        }
+
+        // we can use a queue with pairs to store highest count
+        priority_queue<pair<int, int>> q;
+        for(const auto & pair : numsFreq){
+            q.push({pair.second, pair.first});
+        }
+
         vector<int> results;
         for(int i = 0; i < k; i++){
             results.push_back(q.top().second);
@@ -25,6 +26,7 @@ public:
         }
 
         return results;
-        
+
+
     }
 };
