3569fa64c3cfb66fea2b84653a0b3fb1e147de2c
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-02-08 10:32:33 -0600

    Time: 12 ms (83.04%), Space: 24.9 MB (78.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-        unordered_map<string, vector<string>> myMap;
-        vector<vector<string>> results;
+
+        vector<vector<string>> result;
+
+        // strs = ["eat","tea","tan","ate","nat","bat"]
+        // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+        // We can have a map -> sorted(key) : vector<val>
+
+        unordered_map<string, vector<string>> groups;
 
         for(int i = 0; i < strs.size(); i++){
+
             string word = strs[i];
             sort(word.begin(), word.end());
-            myMap[word].push_back(strs[i]);
+
+            groups[word].push_back(strs[i]);
         }
 
-        for(const auto & pair : myMap){
-            results.push_back(pair.second);
+        for(const auto & pair : groups){
+            result.push_back(pair.second);
         }
 
-        return results;
+        return result;
+        
     }
 };
