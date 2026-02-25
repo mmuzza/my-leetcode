958779fd765271136d7caccccded24d26e396950
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-02-24 23:17:00 -0600

    Time: 0 ms (100%), Space: 10.6 MB (44.57%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+
+    /*
+
+        1   2    3 4   5 6 7
+        16  14  15 10 12 6 7
+
+        2    1     1      2
+        4    3     1      2
+
+
+    */
+
+    if(nums.size() == 1){
+        return nums[0];
+    }
+    if(nums.size() == 2){
+        return max(nums[0], nums[1]);
+    }
+
+
+    vector<int> dp(nums.size(), 0);
+
+    dp[nums.size()-1] = nums[nums.size()-1];
+    dp[nums.size()-2] = nums[nums.size()-2];
+    dp[nums.size()-3] = nums[nums.size()-1] + nums[nums.size()-3];
+
+    for(int i = nums.size()-4; i >= 0; i--){
+        
+        dp[i] = nums[i] + max(dp[i+2], dp[i+3]);
+    }
+
+    return max(dp[0],dp[1]);
+
+
+
+
+
+    
+
+    // int p1 = 0;
+    // int p2 = 0;
+
+    // 2    1     1      2
+    //                   i    p2     p1  both set to 0 initially
+
+    // 2    1     1      2
+    //            i      p2     p1 = 0 and p2 = 2
+
+    // 2    1     1      2
+    //      i     p2     p1  p1 = 2 and p2 = 
+
+
+    // for(int i = nums.size()-1; i >= 0; i--){
+
+    //     int temp = p2;
+    //     p2 = nums[i] + max(p1,p2);
+    //     p1 = p2;
+    // }
+
+        
+    //     return p2;
+
+
+    }
+};
