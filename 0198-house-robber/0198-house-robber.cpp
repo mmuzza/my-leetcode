934b8bb756a3bb9212236a5d6116c46c3734f88f
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-02-24 23:49:02 -0600

    Time: 0 ms (100%), Space: 10.1 MB (89.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -14,54 +14,42 @@ public:
 
     */
 
-    if(nums.size() == 1){
-        return nums[0];
-    }
-    if(nums.size() == 2){
-        return max(nums[0], nums[1]);
-    }
+    // if(nums.size() == 1){
+    //     return nums[0];
+    // }
+    // if(nums.size() == 2){
+    //     return max(nums[0], nums[1]);
+    // }
 
 
-    vector<int> dp(nums.size(), 0);
+    // vector<int> dp(nums.size(), 0);
 
-    dp[nums.size()-1] = nums[nums.size()-1];
-    dp[nums.size()-2] = nums[nums.size()-2];
-    dp[nums.size()-3] = nums[nums.size()-1] + nums[nums.size()-3];
+    // dp[nums.size()-1] = nums[nums.size()-1];
+    // dp[nums.size()-2] = nums[nums.size()-2];
+    // dp[nums.size()-3] = nums[nums.size()-1] + nums[nums.size()-3];
 
-    for(int i = nums.size()-4; i >= 0; i--){
+    // for(int i = nums.size()-4; i >= 0; i--){
         
-        dp[i] = nums[i] + max(dp[i+2], dp[i+3]);
-    }
+    //     dp[i] = nums[i] + max(dp[i+2], dp[i+3]);
+    // }
 
-    return max(dp[0],dp[1]);
+    // return max(dp[0],dp[1]);
 
 
     //-------------------------------------
 
 
 
-    
+    int prev1 = 0;
+    int prev2 = 0;
 
-    
+    for(int num : nums){
+        int temp = prev1;
+        prev1 = max(prev2 + num, prev1);
+        prev2 = temp;
+    }
 
-    // 2    1     1      2
-    //            i           p2     p1  both set to 0 initially
-
-    // 2    1     1      2
-    //      i            p2     p1 = 0 and p2 = 2
-
-    // 2    1     1      2
-    // i          p2     p1  p1 = 2 and p2 = 
-
-
-    // 1       2       3       1
-    //                 i                p2         p1  both set to 0 initially
-
-    // 1       2       3       1
-    //         i               p2         p1       p1 = 0, p2 = 1+0
-
-    // 1       2       3       1
-    // i              p2      p1       p1 = 1, p2 = 2
+    return prev1;
 
 
     
