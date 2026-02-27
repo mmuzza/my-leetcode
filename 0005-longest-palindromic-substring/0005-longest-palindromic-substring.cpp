7aadf96d5b22b8252d85bf503f2b71246041cedf
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-02-27 15:03:50 -0600

    Time: 23 ms (45.46%), Space: 22 MB (36.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,45 +2,36 @@ class Solution {
 public:
     string longestPalindrome(string s) {
 
-        int longestLength = 0;
         string answer = "";
 
         for(int i = 0; i < s.size(); i++){
 
-
             int left = i;
             int right = i;
 
-            while(left >= 0 && right <= s.size()-1 && s[left] == s[right]){
+            while((left >= 0) && (right <= s.size()-1) && (s[left] == s[right])){ 
 
-                if(right-left+1 > longestLength){
-                    longestLength = right-left+1;
-                    answer = s.substr(left, longestLength);
+                if(right-left+1 > answer.size()){
+                    answer = s.substr(left, right-left+1);
                 }
-                
 
                 left--;
                 right++;
             }
 
 
-            
-            // For even lengths we do the exact thing
             left = i;
             right = i+1;
 
-            while(left >= 0 && right <= s.size()-1 && s[left] == s[right]){
+            while((left >= 0) && (right <= s.size()-1) && (s[left] == s[right])){ 
 
-                if(right-left+1 > longestLength){
-                    longestLength = right-left+1;
-                    answer = s.substr(left, longestLength);
+                if(right-left+1 > answer.size()){
+                    answer = s.substr(left, right-left+1);
                 }
-                
 
                 left--;
                 right++;
             }
-
         }
 
         return answer;
