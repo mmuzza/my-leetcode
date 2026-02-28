5d95b2522dad4a27ab76baa1470093c32c9a8ef1
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-02-27 22:34:47 -0600

    Time: 0 ms (100%), Space: 10.3 MB (70.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,37 +3,64 @@ public:
     int rob(vector<int>& nums) {
 
 
+
+
+
     /*
 
         1   2    3 4   5 6 7
         16  14  15 10 12 6 7
 
         2    1     1      2
-        4    3     1      2
+
+                              rob1=0  rob2=0
+i=3                     rob1=2  rob2=0
+i=2              rob1=2  rob2=2
+i=1        rob1=2 rob2=2
+i=0  rob1=4 rob2=2
 
 
     */
 
-    if(nums.size() == 1){
-        return nums[0];
+    int rob1 = 0;
+    int rob2 = 0;
+
+    for(int i = nums.size()-1; i >= 0; i--){
+
+        int temp = max(nums[i] + rob2, rob1);
+        rob2 = rob1;
+        rob1 = temp;
+
     }
-    if(nums.size() == 2){
-        return max(nums[0], nums[1]);
-    }
+
+    return rob1;
 
 
-    vector<int> dp(nums.size(), 0);
 
-    dp[nums.size()-1] = nums[nums.size()-1];
-    dp[nums.size()-2] = nums[nums.size()-2];
-    dp[nums.size()-3] = nums[nums.size()-1] + nums[nums.size()-3];
 
-    for(int i = nums.size()-4; i >= 0; i--){
+
+    /* Solution 1:*/
+
+    // if(nums.size() == 1){
+    //     return nums[0];
+    // }
+    // if(nums.size() == 2){
+    //     return max(nums[0], nums[1]);
+    // }
+
+
+    // vector<int> dp(nums.size(), 0);
+
+    // dp[nums.size()-1] = nums[nums.size()-1];
+    // dp[nums.size()-2] = nums[nums.size()-2];
+    // dp[nums.size()-3] = nums[nums.size()-1] + nums[nums.size()-3];
+
+    // for(int i = nums.size()-4; i >= 0; i--){
         
-        dp[i] = nums[i] + max(dp[i+2], dp[i+3]);
-    }
+    //     dp[i] = nums[i] + max(dp[i+2], dp[i+3]);
+    // }
 
-    return max(dp[0],dp[1]);
+    // return max(dp[0],dp[1]);
 
 
     //-------------------------------------
