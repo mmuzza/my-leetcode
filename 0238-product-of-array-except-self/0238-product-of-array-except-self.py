1f54af2fdbc74be450ee078e2712f4f6bbe305e7
Muhammad Muzzammil <122420196+mmuzza@users.noreply.github.com>
2026-03-29 13:33:09 -0500

    Time: 26 ms (45.55%), Space: 27.8 MB (16.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,18 +7,13 @@ class Solution:
         # left array:    24    12   4    1
         # right array:    1     1   2    6
         # left * right:  24    12   8    6
-   
+
+
         left = [1]
-        right = [1]
+        right = [1] * len(nums)
         output = []
 
         result = 1
-
-        # for n in nums[1:]
-        #     result +=  * result
-        #     left.inser
-
-        #  for i,n in enumerate(nums)
 
         for i in range(1, len(nums)):
             result = nums[i-1] * result
@@ -27,16 +22,38 @@ class Solution:
         result = 1
         for i in range(len(nums)-2,-1,-1):
             result = nums[i+1] * result
-            right.insert(0,result)
-        
-        # print(left)
-        # print(right)
-
+            right[i] = result
+            # right.insert(0,result)
 
         for i in range(len(nums)):
             output.append(left[i] * right[i])
 
         return output
+
+
+
+
+        #########################################
+   
+        # left = [1]
+        # right = [1]
+        # output = []
+
+        # result = 1
+
+        # for i in range(1, len(nums)):
+        #     result = nums[i-1] * result
+        #     left.append(result)
+
+        # result = 1
+        # for i in range(len(nums)-2,-1,-1):
+        #     result = nums[i+1] * result
+        #     right.insert(0,result)
+
+        # for i in range(len(nums)):
+        #     output.append(left[i] * right[i])
+
+        # return output
 
         
 
